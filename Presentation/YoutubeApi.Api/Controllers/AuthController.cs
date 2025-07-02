19174baf96b1cd9407b3d50f9ac55857f316450c
Qorqud Niyazli <niyazli2000@mail.ru>
2025-07-02 13:16:38 +0400

    ders2

---
+++ Presentation/YoutubeApi.Api/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YoutubeApi.Api.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
