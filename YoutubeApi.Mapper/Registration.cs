013d715dadcf26cf14e135992f462bff06a7ec8b
Qorqud Niyazli <niyazli2000@mail.ru>
2025-07-10 15:14:16 +0400

    AutoMapper Created.

---
+++ YoutubeApi.Mapper/Registration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Mapper;
+
+public static class Registration
+{
+    public static void AddCustomMapper(this IServiceCollection services)
+    {
+        services.AddSingleton<YoutubeApi.Application.Interfaces.AutoMapperInterface.IMapper, AutoMapper.Mapper>();
+    }
+}
