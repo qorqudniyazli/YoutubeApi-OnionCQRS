9216817f78865338a565fe57c213ff2961c0bb72
Qorqud Niyazli <niyazli2000@mail.ru>
2025-07-12 17:46:48 +0400

    Minor changens

--- Core/YoutubeApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ Core/YoutubeApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using YoutubeApi.Application.DTOs;
 using YoutubeApi.Application.Interfaces.AutoMapperInterface;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
--- Core/YoutubeApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
+++ Core/YoutubeApi.Application/Features/Products/Queries/GetAllProducts/GetAllProductsQueryResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YoutubeApi.Application.DTOs;
+﻿using YoutubeApi.Application.DTOs;
 
 namespace YoutubeApi.Application.Features.Products.Queries.GetAllProducts;
 
