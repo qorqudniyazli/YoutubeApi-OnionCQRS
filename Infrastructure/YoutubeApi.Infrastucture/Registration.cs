9d04c208bc02d8bc375933aeaec4da772651ad79
Qorqud Niyazli <niyazli2000@mail.ru>
2025-07-14 16:48:41 +0400

    Token service completed and auth interface defined in swagger.

--- Infrastructure/YoutubeApi.Infrastucture/Registration.cs
+++ Infrastructure/YoutubeApi.Infrastucture/Registration.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using YoutubeApi.Application.Interfaces.Tokens;
 using YoutubeApi.Infrastucture.Tokens;
 
 namespace YoutubeApi.Infrastucture;
@@ -9,5 +12,27 @@ public static class Registration
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<TokenSettings>(configuration.GetSection("JWT"));
+        services.AddTransient<ITokenService, TokenService>();
+
+        services.AddAuthentication(opt =>
+        {
+            opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+        {
+            opt.SaveToken = true;
+            opt.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    System.Text.Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
+                ValidateLifetime = true,
+                ValidIssuer = configuration["JWT:Issuer"],
+                ValidAudience = configuration["JWT:Audience"],
+                ClockSkew = TimeSpan.Zero
+            };
+        });
     }
 }
